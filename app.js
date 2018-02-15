8003cb9fac4b1c6da4022317940f0b726099b310
Amy <amyewitt@gmail.com>
2018-02-15 17:44:55 -0600

    stuck on remove li

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+  const deleteItem = document.getElementById('deleteItem')
 
   addToDoForm.addEventListener('submit',  event => {
     event.preventDefault();
@@ -18,11 +19,23 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create a new input
+    let button = document.createElement('button');
+
+    // set the input's type to button
+    button.type = "button";
+
+    // label button as x (delete)
+    button.innerHTML = 'X';
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    // attach the delete button to the li
+    newLi.appendChild(button);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
@@ -30,7 +43,10 @@ function onReady() {
     // empty the input
     newToDoText.value = '';
 
+    $('#newLi').remove();
+
   });
+
 }
 
 window.onload = function() {
